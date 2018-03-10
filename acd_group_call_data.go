2d5b4fd785a58c308195f86214f15a9a4e70b554
ingemar <robin.nilsson@mitel.com>
2018-03-10 23:44:57 +0100

    Added documentation

--- acd_group_call_data.go
+++ acd_group_call_data.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+// AcdGroupServ - The parameter states whether direct indialling traffic is permitted/not permitted to the
+// group number, whether the A-party will be charged or not on answer and if request of A-number will be
+// done or not (only for Swiss MFC DID-trunks), the selection of music-on-wait option, and if the ACD
+// group is classified as a CTI group and if the ACD group is permitted to accept collect calls (Brazil)
+// and if automatic extending to ACD group queues is permitted or not.
 type AcdGroupServ struct {
 	DirectIndialingTraffic                                bool `json:"directIndialingTraffic"`
 	ASubscriberWillNotBeChargedAndNoANumberRequested      bool `json:"aSubscriberWillNotBeChargedAndNoANumberRequested"`
@@ -23,12 +28,23 @@ type AcdGroupServ struct {
 }
 
 type AcdGroupSel struct {
-	SequentialSearching                       bool   `json:"sequentialSearching"`
-	SequentialSearchingToFreeMembers          bool   `json:"sequentialSearchingToFreeMembers"`
-	QueueInternalCallsTowardsTheGroup         bool   `json:"queueInternalCallsTowardsTheGroup"`
-	OverflowPermittedNoAvailableMembersOrFull bool   `json:"overflowPermittedNoAvailableMembersOrFull"`
-	OverflowPermittedWhenFull                 bool   `json:"overflowPermittedWhenFull"`
-	OverflowNotPermitted                      bool   `json:"overflowNotPermitted"`
+	// Member Selection Order. Whether searching for a free member in the group should be undertaken in
+	// the order in which the group members were initiated into the ACD group or according to the group
+	// member who has been free for the longest time.
+	SequentialSearching              bool `json:"sequentialSearching"`
+	SequentialSearchingToFreeMembers bool `json:"sequentialSearchingToFreeMembers"`
+
+	// Internal Queuing. Whether internal calls towards the ACD group should be queued or not.
+	QueueInternalCallsTowardsTheGroup bool `json:"queueInternalCallsTowardsTheGroup"`
+
+	// Overflow. Whether calls towards the ACD group are permitted or not permitted to be diverted.
+	OverflowPermittedNoAvailableMembersOrFull bool `json:"overflowPermittedNoAvailableMembersOrFull"`
+	OverflowPermittedWhenFull                 bool `json:"overflowPermittedWhenFull"`
+	OverflowNotPermitted                      bool `json:"overflowNotPermitted"`
+
+	// External Overflow/Follow Me. Maximum number of simultaneous calls, which are overflowed to an external
+	// destination. Overflow occurs either when there are no queue positions or when all members of the
+	// ACD group are unavailable or at both of those conditions.
 	MaxNumberOfOverflowsToExternalDestination string `json:"maxNumberOfOverflowsToExternalDestination"`
 }
 
