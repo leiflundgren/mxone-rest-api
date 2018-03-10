5da6a67ada89b8bacc78e3cd1c648080596ff398
ingemar <robin.nilsson@mitel.com>
2018-03-10 20:57:15 +0100

    Added parsing for number conversion data

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func main() {
-	asd := NumberConversionEntries{}
-	asd.Get()
+	asd := NumberConversionEntry{}
+	lol := asd.Get()
+
+	brap, err := json.MarshalIndent(lol, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(brap))
 }
--- number_conversion.go
+++ number_conversion.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 // NumberConversionEntry is used for printing data from the number conversion and bearer Capability and High-Level Compatibility substitution tables.
 type NumberConversionEntry struct {
-	conversionType    int
-	entry             string
-	numberType        int
-	pre               string
-	route             int
-	targetDestination string
+	ConversionType    int    `json:"conversionType"`
+	Entry             string `json:"entry"`
+	NumberType        int    `json:"numberType"`
+	Pre               string `json:"pre"`
+	Route             int    `json:"route"`
+	TargetDestination string `json:"targetDestination"`
 }
-
-// NumberConversionEntries is an array of Number Conversion entries
-type NumberConversionEntries []NumberConversionEntry
 
 // Conversion Type
 const (
@@ -47,11 +48,94 @@ const (
 	internalDirectoryContinue             = 12
 )
 
-func (this *NumberConversionEntries) Get() {
-
+func (this *NumberConversionEntry) Get() []NumberConversionEntry {
+	return this.readFromFile()
 }
 
-func (this *NumberConversionEntries) execAndParse() {
+func (this *NumberConversionEntry) parse(byteArray []byte) []NumberConversionEntry {
+	var tempVar string
+	var linesToSkip = 2
+	var skippedLines = 0
+	var result []NumberConversionEntry
+
+	scanner := bufio.NewScanner(strings.NewReader(string(byteArray)))
+	for scanner.Scan() {
+		// Skipping the table header
+		if skippedLines <= linesToSkip {
+			skippedLines++
+			continue
+		}
+
+		fmt.Println(scanner.Text())
+		runes := []rune(scanner.Text())
+		runesLength := len(runes)
+
+		var entry string
+		if runesLength >= 20 {
+			entry = string(runes[0:20])
+			entry = strings.TrimSpace(entry)
+		}
+
+		var conversionType int
+		if runesLength >= 22 {
+			tempVar = string(runes[21:22])
+			tempVar = strings.TrimSpace(tempVar)
+
+			conversionType, _ = strconv.Atoi(tempVar)
+		}
+
+		var numberType int
+		if runesLength >= 29 {
+			tempVar = string(runes[28:29])
+			tempVar = strings.TrimSpace(tempVar)
+
+			numberType, _ = strconv.Atoi(tempVar)
+		}
+
+		var route int
+		if runesLength >= 36 {
+			tempVar = string(runes[35:39])
+			tempVar = strings.TrimSpace(tempVar)
+
+			route, _ = strconv.Atoi(tempVar)
+		}
+
+		var targetDestination string
+		if runesLength >= 40 {
+			targetDestination = string(runes[39:47])
+			targetDestination = strings.TrimSpace(targetDestination)
+		}
+
+		var pre string
+		if runesLength >= 48 {
+			pre = string(runes[47:68])
+			pre = strings.TrimSpace(pre)
+		}
+
+		numberConversionEntry := NumberConversionEntry{}
+		numberConversionEntry.ConversionType = conversionType
+		numberConversionEntry.Entry = entry
+		numberConversionEntry.Pre = pre
+		numberConversionEntry.TargetDestination = targetDestination
+		numberConversionEntry.Route = route
+		numberConversionEntry.NumberType = numberType
+
+		result = append(result, numberConversionEntry)
+	}
+
+	return result
+}
+
+func (this *NumberConversionEntry) readFromFile() []NumberConversionEntry {
+	byteArray, err := ioutil.ReadFile("number_conversion_print")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return this.parse(byteArray)
+}
+
+func (this *NumberConversionEntry) execAndParse() {
 	cmd := exec.Command("number_conversion_print")
 
 	var bytesResult bytes.Buffer
@@ -62,4 +146,14 @@ func (this *NumberConversionEntries) execAndParse() {
 	}
 
 	fmt.Println(bytesResult.String())
+	scanner := bufio.NewScanner(strings.NewReader(bytesResult.String()))
+	for scanner.Scan() {
+		runes := []rune(scanner.Text())
+		fmt.Println(string(runes[0:21]))
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Fan också")
+	}
 }
